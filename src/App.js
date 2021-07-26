b9da4de5fcfca35d2d60a89c10908334a24a952a
Paula Castelan <paulacastelanf@gmail.com>
2021-07-25 21:00:01 -0300

    starter react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <input placeholder="Usuário" name="usuario"/>
+      <button type="button">Pesquisar</button>
+    </>
+  );
+}
+
+export default App;
